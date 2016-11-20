c56f8affe7094bf50d1c1475df30ca4b6b218783
Mykola Makhin <Mykola.Makhin@symphony-solutions.eu>
2016-11-20 21:42:40 +0200

    Updated to use Domoticz JSON for a new demo.

--- src/main/java/x/mvmn/util/rpi/gsmutil/GSMUtil.java
+++ src/main/java/x/mvmn/util/rpi/gsmutil/GSMUtil.java
@@ -1,6 +1,8 @@
 package x.mvmn.util.rpi.gsmutil;
 
 import java.io.UnsupportedEncodingException;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Properties;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -8,9 +10,13 @@ import java.util.logging.Logger;
 import org.eclipse.paho.client.mqttv3.MqttException;
 import org.eclipse.paho.client.mqttv3.MqttSecurityException;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
 public class GSMUtil implements MqttMessageListener {
 
 	private static final Logger LOGGER = Logger.getLogger(GSMUtil.class.getName());
+	private static final Gson GSON = new GsonBuilder().setPrettyPrinting().create();
 
 	protected final Properties properties = new Properties();
 	protected final MqttHandler mqttHandler;
@@ -49,8 +55,11 @@ public class GSMUtil implements MqttMessageListener {
 	}
 
 	public void startService() throws MqttSecurityException, MqttException {
+		System.out.println("Connection to MQTT broker");
 		mqttHandler.connect();
+		System.out.println("Subscribing to topics");
 		mqttHandler.subscribe(properties.getProperty("mqtt.subscribe.topic"));
+		System.out.println("Ensuring GSM module is on");
 		int retries = 3;
 		boolean done = false;
 		do {
@@ -65,6 +74,7 @@ public class GSMUtil implements MqttMessageListener {
 		}
 		ThreadHelper.ensuredSleep(500);
 		sim900helper.clearResponses();
+		System.out.println("Setting SMS mode to text");
 		sim900helper.cmdSetSMSModeText();
 		waitTime = 5;
 		while (sim900helper.responses.size() < 2 && waitTime-- > 0) {
@@ -72,82 +82,90 @@ public class GSMUtil implements MqttMessageListener {
 		}
 		ThreadHelper.ensuredSleep(500);
 		sim900helper.clearResponses();
+
+		System.out.println("Setting SMS encoding to GSM");
 		sim900helper.cmdSetSMSEncodingGSM();
 		waitTime = 5;
 		while (sim900helper.responses.size() < 2 && waitTime-- > 0) {
 			ThreadHelper.ensuredSleep(1000);
 		}
 		ThreadHelper.ensuredSleep(500);
+		System.out.println("Finished initializing. GSM module indicated working: " + done);
 	}
 
 	public void messageArrived(String topic, byte[] body) {
 		try {
-			topic = topic.toLowerCase();
-			final String bodyStr = new String(body, "UTF-8");
+			@SuppressWarnings("unchecked")
+			Map<String, Object> payload = (Map<String, Object>) GSON.fromJson(new String(body, "UTF-8"), Map.class);
 
-			if (topic.startsWith(properties.getProperty("mqtt.subscribe.topic").replaceAll("#", ""))) {
-				final String command = topic.substring(properties.getProperty("mqtt.subscribe.topic").replaceAll("#", "").length());
-				if (command.equals("power")) {
-					if (bodyStr.equalsIgnoreCase("on")) {
-						System.out.println("Ensure on: " + sim900helper.ensureOn());
-					} else if (bodyStr.equalsIgnoreCase("off")) {
-						System.out.println("Ensure on before toggle: " + sim900helper.ensureOn());
-						Sim900Helper.toggleOnOff();
-					} else if (bodyStr.equalsIgnoreCase("toggle")) {
-						Sim900Helper.toggleOnOff();
-					} else {
-						LOGGER.log(Level.WARNING, "Unknown sim900 power command: " + bodyStr);
-					}
-				} else if (command.equals("sms/send")) {
-					final int separatorIndex = bodyStr.indexOf(" ");
-					final String number = bodyStr.substring(0, separatorIndex);
-					final String text = bodyStr.substring(separatorIndex + 1);
+			// int idx = Integer.parseInt(payload.get("idx").toString());
+			int nvalue = Integer.parseInt(payload.get("nvalue").toString());
+			String name = payload.get("name").toString();
 
-					sim900helper.clearResponses();
-					sim900helper.cmdSendSMSMessage(number, text);
-					int waitTime = 5;
-					while (sim900helper.responses.size() < 1 && waitTime-- > 0) {
-						ThreadHelper.ensuredSleep(1000);
-					}
-					sim900helper.clearResponses();
-					ThreadHelper.ensuredSleep(500);
-				} else if (command.equals("getbalance")) {
-					sim900helper.clearResponses();
-					sim900helper.cmdDial("*111#");
-					int waitTime = 5;
-					while (sim900helper.responses.size() < 2 && waitTime-- > 0) {
-						ThreadHelper.ensuredSleep(1000);
-					}
-					sim900helper.pollResponses();
-					sim900helper.pollResponses();
-					ThreadHelper.ensuredSleep(1000);
-					waitTime = 5;
-					while (sim900helper.responses.size() < 1 && waitTime-- > 0) {
-						ThreadHelper.ensuredSleep(1000);
-					}
-					StringBuffer result = new StringBuffer();
-					String line = null;
-					do {
-						line = sim900helper.responses.poll();
-						if (line != null) {
-							result.append(line).append(" ");
-						}
-					} while (line != null);
-					String balanceResult = result.toString().replaceAll(".*Na rahunku\\s+([\\d\\\\.]+)\\s+grn.*", "$1");
-					float balance = -1f;
-					try {
-						balance = Float.parseFloat(balanceResult);
-					} catch (NumberFormatException nfe) {
-						LOGGER.log(Level.WARNING, "Failed to parse balance response: " + balanceResult);
-					}
-					try {
-						mqttHandler.send(properties.getProperty("mqtt.publish.topic") + "balance", "" + balance, null);
-					} catch (Exception e) {
-						LOGGER.log(Level.SEVERE, "Failed to send MQTT message", e);
-					}
-				} else if (command.equals("call")) {
-					sim900helper.cmdDial(bodyStr.trim() + ";");
+			if (name.equals(properties.getProperty("sensor.sim900.name"))) {
+				if (nvalue == 1) {
+					System.out.println("Ensure on: " + sim900helper.ensureOn());
+				} else if (nvalue == 0) {
+					System.out.println("Ensure on before toggle: " + sim900helper.ensureOn());
+					Sim900Helper.toggleOnOff();
+				} else if (nvalue == 2) {
+					Sim900Helper.toggleOnOff();
+				} else {
+					LOGGER.log(Level.WARNING, "Unknown sim900 power command: " + nvalue);
 				}
+			} else if (name.equals(properties.getProperty("sensor.door.name")) && nvalue == 0) {
+				final String number = properties.getProperty("phonenum");
+				final String text = properties.getProperty("sensor.door.msgtext", "Door was open!");
+
+				sim900helper.clearResponses();
+				sim900helper.cmdSendSMSMessage(number, text);
+				int waitTime = 5;
+				while (sim900helper.responses.size() < 1 && waitTime-- > 0) {
+					ThreadHelper.ensuredSleep(1000);
+				}
+				sim900helper.clearResponses();
+				ThreadHelper.ensuredSleep(500);
+			} else if (name.equals(properties.getProperty("sensor.balance.name"))) {
+				sim900helper.clearResponses();
+				sim900helper.cmdDial("*111#");
+				int waitTime = 5;
+				while (sim900helper.responses.size() < 2 && waitTime-- > 0) {
+					ThreadHelper.ensuredSleep(1000);
+				}
+				sim900helper.pollResponses();
+				sim900helper.pollResponses();
+				ThreadHelper.ensuredSleep(1000);
+				waitTime = 5;
+				while (sim900helper.responses.size() < 1 && waitTime-- > 0) {
+					ThreadHelper.ensuredSleep(1000);
+				}
+				StringBuffer result = new StringBuffer();
+				String line = null;
+				do {
+					line = sim900helper.responses.poll();
+					if (line != null) {
+						result.append(line).append(" ");
+					}
+				} while (line != null);
+				String balanceResult = result.toString().replaceAll(".*Na rahunku\\s+([\\d\\\\.]+)\\s+grn.*", "$1");
+				float balance = -1f;
+				try {
+					balance = Float.parseFloat(balanceResult);
+				} catch (NumberFormatException nfe) {
+					LOGGER.log(Level.WARNING, "Failed to parse balance response: " + balanceResult);
+				}
+				try {
+					Map<String, Object> balanceMessage = new HashMap<String, Object>();
+					balanceMessage.put("svalue", String.valueOf(balance));
+					balanceMessage.put("nvalue", 0);
+					balanceMessage.put("idx", Integer.parseInt(properties.getProperty("mqtt.balance.idx", "1")));
+					mqttHandler.send(properties.getProperty("mqtt.publish.topic"), GSON.toJson(balanceMessage), null);
+				} catch (Exception e) {
+					LOGGER.log(Level.SEVERE, "Failed to send MQTT message", e);
+				}
+			} else if (name.equals(properties.getProperty("sensor.bell.name")) && nvalue == 1) {
+				final String number = properties.getProperty("phonenum");
+				sim900helper.cmdDial(number + ";");
 			}
 		} catch (UnsupportedEncodingException e) {
 			throw new RuntimeException(e);
